c2429d893fe18419f8aa4790711d7060d3159d96
TheDrakonir <jannes@opicht.de>
2021-02-13 23:51:54 +0100

    Add random agent

---
+++ src/main/java/de/opicht/ft/facharbeit/agents/RandomAgent.java
@@ -0,0 +1,27 @@
+package de.opicht.ft.facharbeit.agents;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+import de.opicht.ft.facharbeit.game.BoardState;
+import de.opicht.ft.facharbeit.game.Move;
+import de.opicht.ft.facharbeit.game.PositionState;
+
+public class RandomAgent implements Agent {
+
+    @Override
+    public String getAgentIdentifier() {
+        return "Random";
+    }
+
+    @Override
+    public Move determineNextMove(PositionState[][] board) {
+        while (true) {
+            int[] randomIndecies = ThreadLocalRandom.current().ints(2, 0, BoardState.BOARD_SIZE).toArray();
+            
+            if (board[randomIndecies[0]][randomIndecies[1]] == PositionState.EMPTY) {
+                return new Move(randomIndecies[0], randomIndecies[1]);
+            }
+        }
+    }
+
+}
