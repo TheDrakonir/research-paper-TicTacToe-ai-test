fbfc4246a0fc74174625b211c20660c14216d33e
TheDrakonir <jannes@opicht.de>
2021-03-08 23:06:48 +0100

    Add move documentation

--- src/main/java/de/opicht/ft/facharbeit/game/Move.java
+++ src/main/java/de/opicht/ft/facharbeit/game/Move.java
@@ -5,6 +5,10 @@ public class Move {
     public final int targetRow;
     public final int targetColumn;
 
+    /**
+     * @param targetRow (zero-indexed field)
+     * @param targetColumn (zero-indexed field)
+     */
     public Move(int targetRow, int targetColumn) {
         this.targetRow = targetRow;
         this.targetColumn = targetColumn;
