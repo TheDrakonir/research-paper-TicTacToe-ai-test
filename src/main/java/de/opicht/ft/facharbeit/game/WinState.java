d9b8ea8ec89fea4ef83a931b8ac575a8ee345928
TheDrakonir <jannes@opicht.de>
2021-03-08 21:33:44 +0100

    Add WinState equals method

--- src/main/java/de/opicht/ft/facharbeit/game/WinState.java
+++ src/main/java/de/opicht/ft/facharbeit/game/WinState.java
@@ -16,4 +16,27 @@ public class WinState {
         return new WinState(isWon || other.isWon, winner.or(() -> other.winner));
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (isWon ? 1231 : 1237);
+        result = prime * result + winner.hashCode();
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        WinState other = (WinState) obj;
+        if (isWon != other.isWon)
+            return false;
+        return winner.equals(other.winner);
+    }
+
 }
