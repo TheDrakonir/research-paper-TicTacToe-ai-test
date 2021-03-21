b695313a884f00af1608d42eb1aed1f2377339af
TheDrakonir <jannes@opicht.de>
2021-03-21 21:15:35 +0100

    Add example main function

--- src/main/java/de/opicht/ft/facharbeit/App.java
+++ src/main/java/de/opicht/ft/facharbeit/App.java
@@ -1,9 +1,15 @@
 package de.opicht.ft.facharbeit;
 
+import de.opicht.ft.facharbeit.agents.Agent;
+import de.opicht.ft.facharbeit.agents.MonteCarloTreeSearchAgent;
+import de.opicht.ft.facharbeit.agents.RandomAgent;
+
 public class App {
 
     public static void main(String[] args) {
-        
+        Agent[] agents = new Agent[] { new RandomAgent(), new MonteCarloTreeSearchAgent() };
+
+        new GameDispatcher(100, agents);
     }
 
 }
