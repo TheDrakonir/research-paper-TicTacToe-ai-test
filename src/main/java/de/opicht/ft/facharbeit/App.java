33a5c828787d1128007d55de6d690105facaa360
TheDrakonir <jannes@opicht.de>
2021-03-23 12:14:48 +0100

    Add app utility method

--- src/main/java/de/opicht/ft/facharbeit/App.java
+++ src/main/java/de/opicht/ft/facharbeit/App.java
@@ -1,15 +1,31 @@
 package de.opicht.ft.facharbeit;
 
+import java.time.Duration;
+import java.time.Instant;
+
 import de.opicht.ft.facharbeit.agents.Agent;
+import de.opicht.ft.facharbeit.agents.MinimaxAgent;
+import de.opicht.ft.facharbeit.agents.MinimaxWithPruningAgent;
 import de.opicht.ft.facharbeit.agents.MonteCarloTreeSearchAgent;
 import de.opicht.ft.facharbeit.agents.RandomAgent;
 
 public class App {
 
     public static void main(String[] args) {
-        Agent[] agents = new Agent[] { new RandomAgent(), new MonteCarloTreeSearchAgent() };
+        Agent[] agents = new Agent[] { new MinimaxAgent(), new MonteCarloTreeSearchAgent() };
+        benchmarkGames(100, agents);
 
-        new GameDispatcher(100, agents);
+        Agent[] agents2 = new Agent[] { new MinimaxWithPruningAgent(), new MonteCarloTreeSearchAgent() };
+        benchmarkGames(100, agents2);
+    }
+
+    public static void benchmarkGames(int gameCount, Agent[] agents) {
+        Instant start = Instant.now();
+        new GameDispatcher(gameCount, agents);
+        Instant end = Instant.now();
+
+        Duration duration = Duration.between(start, end);
+        System.out.println(duration.toString().substring(2));
     }
 
 }
